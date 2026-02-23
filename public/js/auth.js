824ba7a34b3b65558496f5ad4c409fb1ff5c675e
dolft <tanrd@farmingdale.edu>
2026-02-23 17:17:21 -0500

    You can go back to home page nowww

--- public/js/auth.js
+++ public/js/auth.js
@@ -53,7 +53,6 @@ export function setupLogin() {
             });
         }
 
-        window.location.href = "dashboard.php";
-            window.location.href = "../application/home.html";
+        window.location.href = "../application/home.html";
     });
 }
