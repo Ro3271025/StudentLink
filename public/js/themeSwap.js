a4666fa3e6b6e3283da3a87d8f280c50fbe4f3ba
Peyton <ivegotfrostbyte@gmail.com>
2026-02-24 14:14:18 -0500

    Add theme persistence

--- public/js/themeSwap.js
+++ public/js/themeSwap.js
@@ -1,29 +1,33 @@
+
 function changeTheme(){ 
     var rootCSS = document.querySelector(':root');
-    var rootComputed = getComputedStyle(rootCSS); // debug
 
     var selBox = document.getElementById("themeSelector");
     var selValue = selBox.options[selBox.selectedIndex].value;
-
-    console.log(rootComputed.getPropertyValue('--theme-accent')); //debug
+    localStorage.setItem("storedAccent", selValue);
 
     rootCSS.style.setProperty('--theme-accent', selValue);
 
     switch (selValue) { // theres a better way to do this I just don't have the time or patience right now - PR
         case "#0f73ff": // blue
             rootCSS.style.setProperty('--button-hover', "#113D7A");
+            localStorage.setItem("storedHover", "#113D7A");
             break;
         case "#A11010": // red
             rootCSS.style.setProperty('--button-hover', "#4A0808");
+            localStorage.setItem("storedHover", "#4A0808");
             break;
         case "#E86C13": // orange
             rootCSS.style.setProperty('--button-hover', "#99490F");
+            localStorage.setItem("storedHover", "#99490F");
             break;
         case "#A947C9": // purple
             rootCSS.style.setProperty('--button-hover', "#4D205C");
+            localStorage.setItem("storedHover", "#4D205C");
             break;
         case "#004a41": // green
             rootCSS.style.setProperty('--button-hover', "#01241F");
+            localStorage.setItem("storedHover", "#01241F");
             break;
     }
 
@@ -40,16 +44,32 @@ function changeBG(){
     switch (selValue){
         case "#151D28": // Dark
             rootCSS.style.setProperty('--text-fill', "#ffffff");
+            localStorage.setItem("storedBG", "#151D28");
             break;
         case "#000000": // Lights out
             rootCSS.style.setProperty('--text-fill', "#ffffff");
+            localStorage.setItem("storedBG", "#000000");
             break;
         case "#F2F2EB": // Light
             rootCSS.style.setProperty('--text-fill', "#000000");
+            localStorage.setItem("storedBG", "#F2F2EB");
             break;
     }
 }
 
+function reloadTheme(accent, hoverAccent, BG){
+    var rootCSS = document.querySelector(':root');
+
+    rootCSS.style.setProperty('--bg-primary', BG); // background
+    rootCSS.style.setProperty('--theme-accent', accent); // primary accent
+    rootCSS.style.setProperty('--button-hover', hoverAccent); // on hover accent
+
+}
+
+// retain the user's theme across refresh
+document.addEventListener('DOMContentLoaded', reloadTheme(localStorage.getItem("storedAccent"), localStorage.getItem("storedHover"), localStorage.getItem("storedBG")));
+console.log(localStorage.getItem("storedAccent"));
+console.log(localStorage.getItem("storedHover"));
 
 
 
