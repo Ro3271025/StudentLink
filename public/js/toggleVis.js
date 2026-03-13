0fa505bc42470b417afa4c77572408df3f8934e2
Peyton <ivegotfrostbyte@gmail.com>
2026-03-13 15:12:00 -0400

    Remove Debug

--- public/js/toggleVis.js
+++ public/js/toggleVis.js
@@ -1,9 +1,6 @@
 function toggleOverlay(target){
     var targetItem = document.getElementById(target);
-
     var rootCSS = document.querySelector(':root');
-
-    console.log(targetItem.style.visibility);
     
     if(targetItem.style.visibility == 'hidden' || targetItem.style.visibility == ""){ // the calculated CSS style isn't sent to the script unless set as inline, so check for ""
         targetItem.style.visibility = 'visible'; // I have to fix this later
