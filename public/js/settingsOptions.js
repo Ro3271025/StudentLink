d7efc0997c96bb96ed467aba198ff5ebd03906f0
Christopher Gaguancela <gagucr@farmingdale.edu>
2026-03-29 23:59:04 -0400

    settings fixes

--- public/js/settingsOptions.js
+++ public/js/settingsOptions.js
@@ -1,111 +1,179 @@
-// Account Options
+// Settings
+import { auth, db } from "./firebaseInitialization.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+// LOAD USERNAME 
+onAuthStateChanged(auth, async (user) => {
+    if (!user) return;
+
+    const userRef = doc(db, "users", user.uid);
+    const snap = await getDoc(userRef);
+
+    if (snap.exists()) {
+        const data = snap.data();
+
+        const headerUsername = document.getElementById("headerUsername");
+        if (headerUsername) {
+            headerUsername.textContent = "@" + (data.username || "user");
+        }
+    }
+});
+
+// STATE VARIABLES 
+let accOpen = false;
+let themeOpen = false;
+let aboutOpen = false;
+
+// ACCOUNT OPTIONS
 function expandAcc() {
     const accOpt = document.getElementById('accOption');
 
-    /* Im sure theres a better comparison but this is the easiest imo */
-
-    if(accOpt.innerHTML.length > 204) { // exact length of the base option
-        accOpt.innerHTML = "<p id='accOption'><button class='openBtn optionTxt' onclick='expandAcc()'>Your Account<br><small class='smallTxt'>"+
-        "See information about your account and update details</small></button></p>"
+    if (accOpen) {
+        accOpt.innerHTML = `
+            <button class='openBtn optionTxt' onclick='expandAcc()'>
+                Your Account<br>
+                <small class='smallTxt'>
+                    See information about your account and update details
+                </small>
+            </button>
+        `;
+        accOpen = false;
     } else {
-        accOpt.innerHTML = "<button class='openBtn optionTxt' onclick='expandAcc()'>Your Account<br>" +
-        "<small class='smallTxt'>See information about your account and update details</small></button>" +
-        "<div class='settingsOpt'>" + 
-            "<p><strong>Choose a New Username</strong></p>" +
-            "<p>This will display as <strong>@username</strong> on posts and comments.</p>" +
-        
-            "<input class='settingsInput' placeholder='e.g., rodolfo_tan' /> " +
-            "<button class='saveBtn' type='button' style='font-size:12pt; padding:3px'>Save</button>"+
-        "</div>" +
-        "<div class='settingsOpt'>" + 
-            "<p><strong>Change Your Email Address (.edu only)</strong></p>"+
-            "<p>This will be the address you use to sign into the application</p>"+
-        
-            "<input class='settingsInput' placeholder='e.g., jdoe@suny.edu' /> "+
-            "<button class='saveBtn' type='button' style='font-size:12pt; padding:3px'>Save</button>"+
-        "</div>"+
-        "<div class='settingsOpt'>" + 
-            "<p><strong>Log Out</strong></p>" +
-            "<p>Log out of your account on this device</p>" +
-            "<button class='logoutBtn' type='button' onclick='handleLogout()'>Log Out</button><br><br>"+
-        "</div>" +
+        accOpt.innerHTML = `
+            <button class='openBtn optionTxt' onclick='expandAcc()'>
+                Your Account<br>
+                <small class='smallTxt'>
+                    See information about your account and update details
+                </small>
+            </button>
 
-        "<div class='settingsOpt'>" + 
-            "<p><strong>Delete Your Account</strong></p>"+
-            "<p><b><u>WARNING:</u></b> This will permanently delete your account</p>"+
-        
-            "<button class='delBtn' type='button' onclick=confDelete()>Delete Account</button><br><br>"+
-        "</div>"+ 
-        "<div class='separator'></div>";
+            <div class='settingsOpt'>
+                <p><strong>Choose a New Username</strong></p>
+                <p>This will display as <strong>@username</strong></p>
+                <input class='settingsInput' placeholder='e.g., rodolfo_tan' />
+                <button class='saveBtn'>Save</button>
+            </div>
+
+            <div class='settingsOpt'>
+                <p><strong>Change Email (.edu only)</strong></p>
+                <input class='settingsInput' placeholder='e.g., jdoe@suny.edu' />
+                <button class='saveBtn'>Save</button>
+            </div>
+
+            <div class='settingsOpt'>
+                <p><strong>Log Out</strong></p>
+                <button class='logoutBtn' onclick='handleLogout()'>Log Out</button>
+            </div>
+
+            <div class='settingsOpt'>
+                <p><strong>Delete Account</strong></p>
+                <button class='delBtn' onclick='confDelete()'>Delete Account</button>
+            </div>
+        `;
+        accOpen = true;
     }
 }
 
-function confDelete(){
-    var trueUsername = document.getElementById('username').innerText;
-    trueUsername = trueUsername.replace("@", "");
+// DELETE ACCOUNT
+function confDelete() {
+    let trueUsername = document.getElementById('username').innerText.replace("@", "");
 
-    var username = prompt('Are you certain you would like to delete your account?\n\nTo delete your account, type your username below:');
+    let username = prompt(
+        'Are you sure?\n\nType your username to delete your account:'
+    );
 
-    /* Need some firebase code here */
+    if (!username) return;
 
-
-    // sign them out after deletion
-    if(username.toLowerCase == trueUsername.toLowerCase){
-        location.replace('userlogin.html');
+    if (username.toLowerCase() === trueUsername.toLowerCase()) {
+        alert("Account deleted (placeholder)");
+        window.location.href = "login.php";
+    } else {
+        alert("Username does not match.");
     }
 }
 
-// Theme Options
-
+// THEME OPTIONS
 function expandTheme() {
     const themeOpt = document.getElementById('themeOption');
 
-    if(themeOpt.innerHTML.length > 158){
-        themeOpt.innerHTML = "<p><button class='openBtn optionTxt' onclick='expandTheme()'>Appearance<br>" +
-        "<small class='smallTxt'>Change your color scheme or background</small></button></p>"
+    if (themeOpen) {
+        themeOpt.innerHTML = `
+            <button class='openBtn optionTxt' onclick='expandTheme()'>
+                Appearance<br>
+                <small class='smallTxt'>
+                    Change your color scheme or background
+                </small>
+            </button>
+        `;
+        themeOpen = false;
     } else {
-        themeOpt.innerHTML = "<p><button class='openBtn optionTxt' onclick='expandTheme()'>Appearance<br>" +
-        "<small class='smallTxt'>Change your color scheme or background</small></button></p>"+
-        "<div class='settingsOpt'>" +
-        "<p style='font-weight:bold'>Theme</p>" +
-        "<select id='themeSelector' class='themeObject' onchange='changeTheme()'>"+
-            "<option disabled hidden selected>Color</option>"+
-            "<option value='#0f73ff'>Blue</option>"+
-            "<option value='#A11010'>Red</option>"+
-            "<option value='#E86C13'>Orange</option>"+
-            "<option value='#A947C9'>Purple</option>"+
-            "<option value='#004a41'>Green</option>"+
-        "</select> "+      
-        "<select id='bgSelector' class='themeObject' onchange='changeBG()'>"+
-            "<option selected disabled hidden>Background</option>"+
-            "<option value='#000000'>Lights Out</option>"+
-            "<option value='#F2F2EB'>Light</option>"+
-            "<option value='#151D28'>Dark</option>"+
-        "</select><br><br>"+
-    "</div>"+
-    "<div class='separator'></div>";
+        themeOpt.innerHTML = `
+            <button class='openBtn optionTxt' onclick='expandTheme()'>
+                Appearance<br>
+                <small class='smallTxt'>
+                    Change your color scheme or background
+                </small>
+            </button>
+
+            <div class='settingsOpt'>
+                <p><strong>Theme</strong></p>
+
+                <select id='themeSelector' onchange='changeTheme()'>
+                    <option disabled selected>Color</option>
+                    <option value='#0f73ff'>Blue</option>
+                    <option value='#A11010'>Red</option>
+                    <option value='#E86C13'>Orange</option>
+                    <option value='#A947C9'>Purple</option>
+                    <option value='#004a41'>Green</option>
+                </select>
+
+                <select id='bgSelector' onchange='changeBG()'>
+                    <option disabled selected>Background</option>
+                    <option value='#000000'>Lights Out</option>
+                    <option value='#F2F2EB'>Light</option>
+                    <option value='#151D28'>Dark</option>
+                </select>
+            </div>
+        `;
+        themeOpen = true;
     }
 }
 
-
-// "About the app" info - I'm only putting build versions here honestly
-function expandAbout(){
+// ABOUT
+function expandAbout() {
     const aboutOpt = document.getElementById('aboutOption');
 
-    if(aboutOpt.innerHTML.length > 154){
-        aboutOpt.innerHTML = "<p><button class='openBtn optionTxt' onclick='expandAbout()'>About<br>"+
-        "<small class='smallTxt'>View application information</small></button></p>"
+    if (aboutOpen) {
+        aboutOpt.innerHTML = `
+            <button class='openBtn optionTxt' onclick='expandAbout()'>
+                About<br>
+                <small class='smallTxt'>
+                    View application information
+                </small>
+            </button>
+        `;
+        aboutOpen = false;
     } else {
-        aboutOpt.innerHTML = "<p><button class='openBtn optionTxt' onclick='expandAbout()'>About<br>"+
-        "<small class='smallTxt'>View application information</small></button></p>" +
-        "<div class='settingsOpt'>" +
-        "<p style='font-weight:bold'>Application Info</p>"+
-        "<p>App Version: 0.4a</p>"
+        aboutOpt.innerHTML = `
+            <button class='openBtn optionTxt' onclick='expandAbout()'>
+                About<br>
+                <small class='smallTxt'>
+                    View application information
+                </small>
+            </button>
+
+            <div class='settingsOpt'>
+                <p><strong>Application Info</strong></p>
+                <p>App Version: 0.4a</p>
+            </div>
+        `;
+        aboutOpen = true;
     }
 }
-function handleLogout(){
-    // Firebase sign out (if you're using it)
+
+// LOGOUT
+function handleLogout() {
     import("https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js")
     .then(({ signOut }) => {
         import("./firebaseInitialization.js").then(({ auth }) => {
